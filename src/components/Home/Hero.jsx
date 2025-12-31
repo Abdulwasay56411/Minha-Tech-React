870f563ca6503f27e20b663da3df8748772c8842
wasay <wasay56411@gmail.com>
2025-12-31 21:14:02 +0500

    New

---
+++ src/components/Home/Hero.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+  return (
+    <div className="pt-30 px-4 md:px-14">
+      <div className="flex flex-col-reverse lg:flex-row items-center lg:justify-between gap-8">
+        <div className="w-full text-center lg:text-left">
+          <h1 className="font-semibold text-3xl md:text-4xl lg:text-[55px] leading-snug text-black">
+            Building Scalable <br />
+            <span className="text-[#06B8FF]">Mobile Apps</span>
+          </h1>
+
+          <h2 className="mt-4 text-2xl md:text-3xl lg:text-[45px] font-semibold text-black">
+            for the Next Digital Generation
+          </h2>
+
+          <h3 className="mt-2 text-xl md:text-2xl lg:text-3xl font-semibold text-black">
+            Android • iOS • Web • Cloud • AI
+          </h3>
+
+          <p className="mt-4 text-lg text-black">
+            We design, develop, and scale high-performance apps used by
+            thousands of users worldwide.
+          </p>
+
+          <div className="flex gap-4 justify-center lg:justify-start mt-5 flex-wrap">
+            <button className="bg-[#06B8FF] text-white text-sm font-medium px-5 py-2 rounded-lg">
+              Start Your Project
+            </button>
+            <button className="border border-[#06B8FF] text-[#06B8FF] text-sm font-medium px-5 py-2 rounded-lg">
+              View our Project
+            </button>
+          </div>
+        </div>
+
+        {/* Right Image */}
+        <div className="w-full lg:w-[70%] flex justify-center lg:justify-end">
+          <motion.img
+            animate={{y: ["0%", "-8%", "0%"]}}
+            transition={{duration: 2, repeat: Infinity, ease: "easeInOut" }}
+            className="w-149.25 h-auto object-cover"
+            src="/mobile.png"
+            alt="Mobile App Illustration"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from "react";
+import { IoMenu } from "react-icons/io5";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isScroll, setIsScroll] = useState(false);
+
+  const handleMenu = () => {
+    setIsOpen(prev => !prev);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScroll(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <>
+      <nav>
+        <div
+          className={`hidden w-full h-20 py-4 lg:flex z-50  md:fixed items-center px-14 justify-between ${
+            isScroll
+              ? "bg-white/20 backdrop-blur-2xl shadow-md"
+              : "bg-transparent"
+          }`}
+        >
+          <h1 className="text-3xl font-bold text-[#0A1A2C]">Minha Tech</h1>
+          <div>
+            <ul className="flex gap-4">
+              {navLinks.map((item) => {
+                return (
+                  <div key={item.id}>
+                    <li className="cursor-pointer text-[18px] text-[#404040] transition transform duration-300 hover:text-[#06B8FF]">
+                      {item.title}
+                    </li>
+                  </div>
+                );
+              })}
+            </ul>
+          </div>
+          <div>
+            <button className="bg-[#06B8FF] text-white text-[12px] font-semibold w-35.5 h-8.75 rounded-[9px] cursor-pointer ">
+              Publish Your App
+            </button>
+          </div>
+        </div>
+        {/* Mobile responsive menu */}
+        <div
+          className={`lg:hidden w-full z-50 fixed flex items-center justify-between py-4 px-2 ${
+            isScroll
+              ? "bg-white/20 backdrop-blur-2xl shadow-md"
+              : "bg-transparent"
+          }`}
+        >
+          <h1 className="font-bold text-3xl text-[#0A1A2C]">Minha Tech</h1>
+          <div className="relative">
+            <IoMenu
+              onClick={handleMenu}
+              className="text-5xl select-none cursor-pointer"
+            />
+            <div
+              className={`fixed top-14 right-0 bg-green-200 w-52 shadow-md h-full transform transition duration-300 ${
+                !isOpen ? "translate-x-full" : "translate-x-0"
+              }`}
+            >
+              {navLinks.map((item) => {
+                return (
+                  <div key={item.id} className="flex flex-col items-center">
+                    <ul>
+                      <li>{item.title}</li>
+                    </ul>
+                  </div>
+                );
+              })}
+              <div className="flex justify-center">
+                <button className="bg-[#06B8FF] text-white text-[12px] font-semibold w-35.5 h-8.75 rounded-[9px] cursor-pointer ">
+                  Publish Your App
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
+
+const navLinks = [
+  {
+    id: 1,
+    title: "Publishing",
+  },
+  {
+    id: 2,
+    title: "Sell App",
+  },
+  {
+    id: 3,
+    title: "Services",
+  },
+  {
+    id: 4,
+    title: "About",
+  },
+  {
+    id: 5,
+    title: "Careers",
+  },
+  {
+    id: 6,
+    title: "Contact",
+  },
+];
