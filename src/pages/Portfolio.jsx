2b3f645ca3a27313ec0e1b1883a7c1daac8a9f03
wasay <wasay56411@gmail.com>
2026-01-08 21:29:33 +0500

    update

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import PortfolioHero from '../components/Portfolio/PortfolioHero'
+import Testimonials from '../components/Home/Testimonials'
+import PartnesLoves from '../components/PartnerLoves'
+import Footer from '../components/Footer'
 
 const Portfolio = () => {
   return (
     <div>
         <Navbar/>
         <PortfolioHero/>
-        
+        <Testimonials/>
+        <PartnesLoves/>
+        <Footer/>
     </div>
   )
 }
